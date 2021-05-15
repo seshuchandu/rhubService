6c4c455d33f69bbc6f539505c62577aa63545610
seshukumar chandu <seshukumarchandu@gmail.com>
2021-05-15 16:37:45 +0530

    initial Commit

---
+++ src/main/java/com/portal/rhub/RhubApplication.java
@@ -0,0 +1,13 @@
+package com.portal.rhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RhubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RhubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/portal/rhub/RhubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portal.rhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RhubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
